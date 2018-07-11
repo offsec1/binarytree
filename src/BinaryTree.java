bfbd7742d97caf3d79f4d29d4691687c85c49693
anonuser_desktop <offsec1@protonmail.com>
2018-07-11 22:15:35 +0200

    improvements but maybe a recursive logic would be better?

--- src/BinaryTree.java
+++ src/BinaryTree.java
@@ -17,27 +17,54 @@ public class BinaryTree <T> {
         Node newNode = new Node(data);
         tree.add(newNode);
 
-        for (Node n : tree) {
+        //don't process the root element
+        if (tree.size() > 1) {
 
-            if (n.childLeft != null && n.childRight != null)
-                continue;;
+            for (Node n : tree) {
 
-            if (n.childLeft == null)
-                n.childLeft = newNode;
-            else if (n.childRight == null)
-                n.childRight = newNode;
+                if (n.childLeft != null && n.childRight != null)
+                    continue;
+
+                if (n.childLeft == null) {
+                    n.childLeft = newNode;
+                    break;
+                }
+                else if (n.childRight == null) {
+                    n.childRight = newNode;
+                    break;
+                }
+            }
         }
+    }
+
+    public void exportTree() {
+        //TODO PM: export tree to a file (xml would be nice but I guess bin or txt is appropriate for the exam)
+    }
+
+    public void importTree() {
+        //TODO PM: import tree from file
     }
 
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
-        //TODO PM: add output logic
+        sb.append(tree.get(0).toString());
+
+        //TODO PM: add output logic (it's okay for testing but not good like this)
         for (Node n : tree) {
-            sb.append(n.toString());
-            sb.append("   /\t \\\n");
-            
+
+            if (n.childLeft == null && n.childRight == null)
+                break;
+
+            sb.append("\n   /\t \\\n");
+
+            if (n.childLeft != null)
+                sb.append(n.childLeft.toString());
+            if (n.childRight != null)
+                sb.append(n.childRight.toString());
+
+            sb.append("\n");
         }
 
         return sb.toString();
--- src/Test.java
+++ src/Test.java
@@ -1,15 +1,17 @@
 public class Test {
 
     public static void main(String[] args) {
-        System.out.printf("And awayyyyyy we gooooo!\n");
+        //System.out.printf("And awayyyyyy we gooooo!\n");
 
-        String a = "asdf";
+        String a = "asdf", b = "jkl;", c = "qwer", d = "uiop";
         BinaryTree<String> stringTree = new BinaryTree<>();
         stringTree.add(a);
-        stringTree.add(a);
+        stringTree.add(b);
+        stringTree.add(c);
+        stringTree.add(d);
 
-        Integer b = 12;
-        BinaryTree<Integer> integerTree = new BinaryTree<>();
+        //Integer b = 12;
+        //BinaryTree<Integer> integerTree = new BinaryTree<>();
 
         System.out.println(stringTree.toString());
 
